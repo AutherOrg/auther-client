97926ef1027b93ada0cbc2a3c24120b1c0e29925
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-01-10 12:22:42 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 
 import Web3Wrapper from './components/web3/Web3Wrapper'
-import Basic from './components/issuer/Basic'
-// import Validate from './components/issuer/Validate'
-// import Batch from './components/issuer/Batch'
-// import Sign from './components/issuer/Sign'
+import Basic from './components/test/Basic'
 
 export default () => {
   return (
     <Grid container>
       <Grid item xs={12}>
-        {/* <Validate /> */}
-        {/* <Batch /> */}
         <Web3Wrapper>
           <Basic />
-          {/* <Sign /> */}
         </Web3Wrapper>
       </Grid>
     </Grid>
