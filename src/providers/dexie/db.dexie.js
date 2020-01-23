26679a31d0ba25554e23062819917c772852d6cd
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-01-23 17:17:22 +0100

    cleanup

--- src/providers/dexie/db.dexie.js
+++ src/providers/dexie/db.dexie.js
@@ -1,6 +1,7 @@
 import Dexie from 'dexie'
 
 const db = new Dexie('OpenBlockcertsClient')
+
 db.version(1).stores({
   certificates: '++id, status, uuid, json, createdAt, updatedAt, recipientId, issuerId'
 })
