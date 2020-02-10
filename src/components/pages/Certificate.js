b856393c1c5adfa69fa9c3679fb7e7362a5ca786
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-02-10 12:34:50 +0100

    print certificate with QR code

--- src/components/pages/Certificate.js
+++ src/components/pages/Certificate.js
@@ -2,20 +2,33 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import downloadjs from 'downloadjs'
+import ReactToPrint from 'react-to-print'
+import { QRCode } from 'react-qr-svg'
 import {
   Button,
   Card, CardHeader, CardContent, CardActions,
   Grid,
   Typography
 } from '@material-ui/core'
-import { Assignment, Check, CloudDownload, Close, Delete, Link, Share } from '@material-ui/icons'
+import { makeStyles } from '@material-ui/core/styles'
+import { Assignment, Check, CloudDownload, Close, Delete, Link, Print, Share } from '@material-ui/icons'
 
 import certificateActions from '../../actions/certificate.actions'
 import confirmationActions from '../../actions/confirmation.actions'
 import constants from '../../constants/certificates.constants'
 import ConfirmationDialog from '../organisms/ConfirmationDialog'
 
+const useStyles = makeStyles(theme => ({
+  certificateView: {
+    marginTop: '100px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
+}))
+
 export default function Certificate ({ match }) {
+  const classes = useStyles()
   const dispatch = useDispatch()
   const reducer = useSelector(state => state.certificateReducer)
   const [copied, setCopied] = React.useState(null)
@@ -37,6 +50,7 @@ export default function Certificate ({ match }) {
       }, 3000
     )
   }
+  const componentRef = React.useRef()
 
   React.useEffect(() => {
     dispatch(certificateActions.get(match.params.id))
@@ -50,7 +64,27 @@ export default function Certificate ({ match }) {
     <>
       <Grid container spacing={5} justify='center'>
         <Grid item xs={12} lg={6}>
-          <div dangerouslySetInnerHTML={{ __html: reducer.json.displayHtml.replace(/(<? *script)/gi, 'illegalscript') }} />
+          <div ref={componentRef}>
+            <div dangerouslySetInnerHTML={{ __html: reducer.json.displayHtml.replace(/(<? *script)/gi, 'illegalscript') }} />
+            <div className={classes.certificateView}>
+              <Typography variant='h6'>
+                Certificate verification
+              </Typography>
+              <Typography variant='caption'>
+                To verify this certificate, scan this QR code or go to:
+              </Typography>
+              <Typography variant='caption' paragraph>
+                {`${window.location.origin}/certificates/shared/${reducer.uuid}`}
+              </Typography>
+              <QRCode
+                bgColor='#FFFFFF'
+                fgColor='#000000'
+                level='Q'
+                style={{ width: 256 }}
+                value={`${window.location.origin}/certificates/shared/${reducer.uuid}`}
+              />
+            </div>
+          </div>
         </Grid>
         <Grid item xs={12} lg={6}>
           <Grid container spacing={5}>
@@ -86,8 +120,14 @@ export default function Certificate ({ match }) {
                 <Card>
                   <CardHeader title='Sharing' />
                   <CardContent>
+                    <Typography paragraph>
+                      The sharing of this certificate is enabled!
+                    </Typography>
+                    <Typography paragraph>
+                      You can now send the sharing link to any of your contacts.
+                    </Typography>
                     <Typography>
-                      The sharing of this certificate is enabled! You can now send the sharing link to any of your contacts.
+                      You can also print your certificate with a verification link and QR code.
                     </Typography>
                   </CardContent>
                   <CardActions>
@@ -98,7 +138,7 @@ export default function Certificate ({ match }) {
                       startIcon={<Link />}
                       color='primary'
                     >
-                      Open the sharing link
+                      Open link
                     </Button>
                     <CopyToClipboard
                       text={`${window.location.origin}/certificates/shared/${reducer.uuid}`}
@@ -109,9 +149,20 @@ export default function Certificate ({ match }) {
                         startIcon={<Assignment />}
                         color='primary'
                       >
-                        {copied ? 'Link copied to clipboard' : 'Copy the sharing link'}
+                        {copied ? 'Link copied' : 'Copy link'}
                       </Button>
                     </CopyToClipboard>
+                    <ReactToPrint
+                      trigger={() => (
+                        <Button
+                          startIcon={<Print />}
+                          color='primary'
+                        >
+                          Print with QR code and link
+                        </Button>
+                      )}
+                      content={() => componentRef.current}
+                    />
                   </CardActions>
                 </Card>
               </Grid>
