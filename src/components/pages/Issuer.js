ab198d72baea3932ade6f1e4100b293fad062c0a
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-03-20 17:57:33 +0100

    issuer image

--- src/components/pages/Issuer.js
+++ src/components/pages/Issuer.js
@@ -57,8 +57,8 @@ export default function Issuer () {
     if (event.target.files[0]) {
       Resizer.imageFileResizer(
         event.target.files[0],
-        1024,
-        768,
+        200,
+        350,
         'PNG',
         100,
         0,
