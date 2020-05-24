a2a425a78307e0aa78952a2296e4b244b1d92b5e
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-05-24 14:10:52 +0200

    Arial, Helvetica, sans-serif

--- src/templates/default/default.template.js
+++ src/templates/default/default.template.js
@@ -10,7 +10,7 @@ const buildSignatures = signatures => {
     return `
       <div style="display: flex; flex-direction: column; align-items: center; margin: 10px;">
         <img src="${signature.image}" alt="${signature.name}" style="width: auto; height: 50px;" />
-        <p style="font-family: Roboto, Helvetica, Arial, sans-serif; font-style: italic; margin: 0;">
+        <p style="font-family: Arial, Helvetica, sans-serif; font-style: italic; margin: 0;">
           ${signature.name}, ${signature.jobTitle}
         </p>
       </div>
@@ -25,16 +25,16 @@ const build = certificate => {
         <img src="${certificate.badge.image}" alt="${certificate.badge.name}" style="width: 100%; height: auto; margin-bottom: 20px" />
       </div>
       <div style="display: flex; flex-direction: column; align-items: center;">
-        <h1 style="font-family: Roboto, Helvetica, Arial, sans-serif; font-size:3em; font-weight: bold; text-shadow: 3px 3px #e0e0e0; margin: 0; margin-bottom: 20px;">
+        <h1 style="font-family: Arial, Helvetica, sans-serif; font-size:3em; font-weight: bold; text-shadow: 3px 3px #e0e0e0; margin: 0; margin-bottom: 20px;">
           ${certificate.badge.name}
         </h1>
-        <h2 style="font-family: Roboto, Helvetica, Arial, sans-serif; font-size:2.5em; text-transform: uppercase; margin:0; margin-bottom: 20px;">
+        <h2 style="font-family: Arial, Helvetica, sans-serif; font-size:2.5em; text-transform: uppercase; margin:0; margin-bottom: 20px;">
           ${certificate.recipientProfile.name}
         </h2>
-        <p style="font-family: Roboto, Helvetica, Arial, sans-serif; font-style: italic; text-align: justify; margin: 0; margin-bottom: 50px;">
+        <p style="font-family: Arial, Helvetica, sans-serif; font-style: italic; text-align: justify; margin: 0; margin-bottom: 50px;">
           ${certificate.badge.description}
         </p>
-        <p style="font-family: Roboto, Helvetica, Arial, sans-serif; margin: 0; margin-bottom: 50px;">
+        <p style="font-family: Arial, Helvetica, sans-serif; margin: 0; margin-bottom: 50px;">
           Issued on ${format(new Date(certificate.issuedOn), 'yyyy/MM/dd')} by
         </p>
         <div style="display: flex; flex-direction: column; align-items: center;">
