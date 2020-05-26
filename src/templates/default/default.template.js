369552e4d8537c3c655696d1990e0369167ecee4
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-05-26 16:17:59 +0200

    up

--- src/templates/default/default.template.js
+++ src/templates/default/default.template.js
@@ -21,7 +21,7 @@ const buildSignatures = signatures => {
 const build = certificate => {
   const html = `
     <div style="font-family:'Roboto','Helvetica','Arial',sans-serif; text-align:center; background:url(${laurel}) center center no-repeat; background-size:contain;">
-      <h1 style="font-size:400%; font-weight:bold; text-shadow:3px 3px #e0e0e0; margin:0; padding-top:4em;">
+      <h1 style="font-size:400%; font-weight:bold; text-shadow:3px 3px #e0e0e0; margin:0; padding-top:3em;">
         ${certificate.badge.name}
       </h1>
       <h2 style="font-size:300%; text-transform:uppercase; margin:0;">
@@ -33,8 +33,8 @@ const build = certificate => {
       <p style="font-size:100%; font-style:italic; margin:0; margin-top:20px;">
         Issued on ${format(new Date(certificate.issuedOn), 'yyyy/MM/dd')}
       </p>
-      <div style="text-align: center;">
-        <img src="${certificate.badge.issuer.image}" alt="${certificate.badge.issuer.name}" style="max-height:100px; width:auto; margin:0; margin-top:10px;" />
+      <div style="text-align:center;">
+        <img src="${certificate.badge.issuer.image}" alt="${certificate.badge.issuer.name}" style="height:50px; width:auto; margin:0; margin-top:10px;" />
       </div>
       <div style="display:flex; display:-webkit-flex; flex-direction:row; justify-content:center; -webkit-justify-content:center;">
         ${buildSignatures(certificate.badge.signatureLines)}
